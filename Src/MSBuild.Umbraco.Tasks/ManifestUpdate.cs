20424e4d498a6fdaa9696363ee7641c31650f228
Matt Brailsford <527305+mattbrailsford@users.noreply.github.com>
2011-03-23 07:55:18 +0000

    Initial code import

---
+++ Src/MSBuild.Umbraco.Tasks/ManifestUpdate.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Text.RegularExpressions;
+
+namespace MSBuild.Umbraco.Tasks
+{
+    public class ManifestUpdate : Task
+    {
+        private const string VERSION_NO_REGEX = @"^(\d).(\d).(\d)$";
+
+        [Required]
+        public string ManifestFile { get; set; }
+
+        [Required]
+        public string WorkingDirectory { get; set; }
+
+        public string PackageName { get; set; }
+        public string PackageVersion { get; set; }
+        public string PackageLicenseName { get; set; }
+        public string PackageLicenseUrl { get; set; }
+
+        public string MinimumRrequiredUmbracoVersion { get; set; }
+
+        public string AuthorName { get; set; }
+        public string AuthorUrl { get; set; }
+
+        public string Readme { get; set; }
+
+        public ITaskItem[] Files { get; set; }
+
+        public override bool Execute()
+        {
+            try
+            {
+                // Load manifest
+                var doc = new XmlDocument();
+                doc.PreserveWhitespace = true;
+                doc.Load(ManifestFile);
+
+                // Update package info
+                XmlHelper.UpdateNode(ref doc, Constants.PACKAGE_NAME_XPATH, PackageName);
+                XmlHelper.UpdateNode(ref doc, Constants.PACKAGE_VERSION_XPATH, PackageVersion);
+                XmlHelper.UpdateNode(ref doc, Constants.PACKAGE_LICENSE_XPATH, PackageLicenseName);
+                XmlHelper.UpdateAttribute(ref doc, Constants.PACKAGE_LICENSE_XPATH, "url", PackageLicenseUrl);
+
+                // Update requirements
+                if (!string.IsNullOrEmpty(MinimumRrequiredUmbracoVersion) && Regex.IsMatch(MinimumRrequiredUmbracoVersion, VERSION_NO_REGEX))
+                {
+                    var match = Regex.Match(MinimumRrequiredUmbracoVersion, VERSION_NO_REGEX);
+
+                    XmlHelper.UpdateNode(ref doc, Constants.PACKAGE_REQUIREMENTS_XPATH + "/major", match.Groups[1].Value);
+                    XmlHelper.UpdateNode(ref doc, Constants.PACKAGE_REQUIREMENTS_XPATH + "/minor", match.Groups[2].Value);
+                    XmlHelper.UpdateNode(ref doc, Constants.PACKAGE_REQUIREMENTS_XPATH + "/patch", match.Groups[3].Value);
+                }
+
+                // Update author info
+                XmlHelper.UpdateNode(ref doc, Constants.AUTHOR_NAME_XPATH, AuthorName);
+                XmlHelper.UpdateNode(ref doc, Constants.AUTHOR_URL_XPATH, AuthorUrl);
+
+                // Update readme info
+                XmlHelper.UpdateCDataNode(ref doc, Constants.README_XPATH, Readme);
+
+                // Append files
+                if(Files != null && Files.Length > 0)
+                {
+                    Log.LogMessage(Files.Length.ToString() + " files to add to Manifest");
+
+                    var filesNode = doc.SelectSingleNode("//files");
+
+                    foreach (var file in Files)
+                    {
+                        if(File.Exists(file.ItemSpec))
+                        {
+                            var fileInfo = new FileInfo(file.ItemSpec);
+                            if (fileInfo.FullName.StartsWith(WorkingDirectory))
+                            {
+                                var relativeFilePath = fileInfo.FullName.Substring(WorkingDirectory.Length + 1).Replace(@"\", "/");
+                                var relativeDirPath = "/" + relativeFilePath.Substring(0, relativeFilePath.LastIndexOf("/")).Trim('/');
+                                var guid = Guid.NewGuid().ToString() + fileInfo.Extension;
+
+                                var fileNode = XmlHelper.CreateNode(ref doc, "file", "");
+                                var guidNode = XmlHelper.CreateNode(ref doc, "guid", guid);
+                                var orgPathNode = XmlHelper.CreateNode(ref doc, "orgPath", relativeDirPath);
+                                var orgNameNode = XmlHelper.CreateNode(ref doc, "orgName", fileInfo.Name);
+
+                                fileNode.AppendChild(guidNode);
+                                fileNode.AppendChild(orgPathNode);
+                                fileNode.AppendChild(orgNameNode);
+
+                                filesNode.AppendChild(fileNode);
+
+                                fileInfo.MoveTo(fileInfo.DirectoryName + @"\" + guid);
+                            }
+                        }
+                    }
+                }
+
+                // Save manifest
+                doc.Save(ManifestFile);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.LogErrorFromException(e);
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ Src/MSBuild.Umbraco.Tasks/Package.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Xml;
+using ICSharpCode.SharpZipLib.Checksums;
+using ICSharpCode.SharpZipLib.Zip;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.Umbraco.Tasks
+{
+    public class Package : Task
+    {
+        [Required]
+        public string ManifestFile { get; set; }
+
+        [Required]
+        public ITaskItem[] Files { get; set; }
+
+        [Required]
+        public string WorkingDirectory { get; set; }
+
+        [Required]
+        public string OutputDirectory { get; set; }
+
+        protected string _packageFileName;
+        protected string PackageFileName
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(_packageFileName))
+                {
+                    var doc = new XmlDocument();
+                    doc.Load(ManifestFile);
+
+                    var packageNameNode = doc.SelectSingleNode(Constants.PACKAGE_NAME_XPATH);
+                    var packageVersionNode = doc.SelectSingleNode(Constants.PACKAGE_VERSION_XPATH);
+
+                    _packageFileName = Path.Combine(OutputDirectory, packageNameNode.InnerText.Replace(" ", "_") + "_" + packageVersionNode.InnerText + ".zip");
+                }
+
+                return _packageFileName;
+            }
+        }
+
+        public override bool Execute()
+        {
+            return PackageFiles();
+        }
+
+        protected bool PackageFiles()
+        {
+            var flag = false;
+            var crc = new Crc32();
+
+            ZipOutputStream stream = null;
+
+            try
+            {
+                using (stream = new ZipOutputStream(File.Create(PackageFileName)))
+                {
+                    stream.SetLevel(9);
+
+                    var buffer = new byte[0x8000];
+                    foreach (var item in Files)
+                    {
+                        var itemSpec = item.ItemSpec;
+                        var info = new FileInfo(itemSpec);
+
+                        if (!info.Exists)
+                        {
+                            Log.LogWarning("File not found", new object[] { info.FullName });
+                        }
+                        else
+                        {
+                            itemSpec = ZipEntry.CleanName(info.Name, true);
+                            var entry = new ZipEntry(itemSpec);
+                            entry.DateTime = info.LastWriteTime;
+                            entry.Size = info.Length;
+
+                            using (var stream2 = info.OpenRead())
+                            {
+                                crc.Reset();
+                                var length = stream2.Length;
+                                while (length > 0L)
+                                {
+                                    var len = stream2.Read(buffer, 0, buffer.Length);
+                                    crc.Update(buffer, 0, len);
+                                    length -= len;
+                                }
+                                entry.Crc = crc.Value;
+                                stream.PutNextEntry(entry);
+                                length = stream2.Length;
+                                stream2.Seek(0L, SeekOrigin.Begin);
+                                while (length > 0L)
+                                {
+                                    var count = stream2.Read(buffer, 0, buffer.Length);
+                                    stream.Write(buffer, 0, count);
+                                    length -= count;
+                                }
+                            }
+
+                            Log.LogMessage("File added to package", new object[] { itemSpec });
+                        }
+                    }
+                    stream.Finish();
+                }
+
+                Log.LogMessage("Package created", new object[] { PackageFileName });
+                flag = true;
+            }
+            catch (Exception exception)
+            {
+                Log.LogErrorFromException(exception);
+                flag = false;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            return flag;
+        }
+    }
+}
