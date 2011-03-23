e259432af07252151c39cd7ce8fabfda55ecee1b
matt <matt@localhost>
2011-03-23 07:55:18 +0000

    Initial code import

---
+++ Src/MSBuild.Umbraco.Tasks/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSBuild.Umbraco.Tasks
+{
+    public class Constants
+    {
+        internal const string PACKAGE_NAME_XPATH = "umbPackage/info/package/name";
+        internal const string PACKAGE_VERSION_XPATH = "umbPackage/info/package/version";
+        internal const string PACKAGE_LICENSE_XPATH = "umbPackage/info/package/license";
+        internal const string PACKAGE_REQUIREMENTS_XPATH = "umbPackage/info/package/requirements";
+        internal const string AUTHOR_NAME_XPATH = "umbPackage/info/author/name";
+        internal const string AUTHOR_URL_XPATH = "umbPackage/info/author/url";
+        internal const string README_XPATH = "umbPackage/info/readme";
+    }
+}
---
+++ Src/MSBuild.Umbraco.Tasks/XmlHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace MSBuild.Umbraco.Tasks
+{
+    public class XmlHelper
+    {
+        internal static void UpdateNode(ref XmlDocument doc, string xpath, string value)
+        {
+            if (string.IsNullOrEmpty(value)) return;
+
+            var node = doc.SelectSingleNode(xpath);
+            if (node != null)
+            {
+                node.InnerText = value;
+            }
+        }
+
+        internal static void UpdateCDataNode(ref XmlDocument doc, string xpath, string value)
+        {
+            if (string.IsNullOrEmpty(value)) return;
+
+            var node = doc.SelectSingleNode(xpath);
+            if (node != null)
+            {
+                var cDataSection = doc.CreateCDataSection(value);
+                node.InnerXml = cDataSection.OuterXml;
+            }
+        }
+
+        internal static void UpdateAttribute(ref XmlDocument doc, string xpath, string attribute, string value)
+        {
+            if (string.IsNullOrEmpty(value)) return;
+
+            var node = doc.SelectSingleNode(xpath);
+            if (node != null)
+            {
+                var attr = node.Attributes[attribute];
+                if (attr != null)
+                {
+                    attr.Value = value;
+                }
+            }
+        }
+
+        internal static XmlNode CreateNode(ref XmlDocument doc, string name, string value)
+        {
+            var node = doc.CreateNode(XmlNodeType.Element, name, "");
+            node.AppendChild(doc.CreateTextNode(value));
+            return node;
+        }
+    }
+}
