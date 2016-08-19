ef2850a675655af52791234dba69fbcfa9f6b8b7
Jeavon <1829920+Jeavon@users.noreply.github.com>
2016-08-19 13:36:06 +0100

    Moved IconUrl to package element

--- Src/MSBuild.Umbraco.Tasks/Constants.cs
+++ Src/MSBuild.Umbraco.Tasks/Constants.cs
@@ -11,8 +11,8 @@ namespace MSBuild.Umbraco.Tasks
         internal const string PACKAGE_URL = "umbPackage/info/package/url";
         internal const string PACKAGE_LICENSE_XPATH = "umbPackage/info/package/license";
         internal const string PACKAGE_REQUIREMENTS_XPATH = "umbPackage/info/package/requirements";
-        internal const string AUTHOR_NAME_XPATH = "umbPackage/info/author/name";
-        internal const string AUTHOR_ICON_URL = "umbPackage/info/author/iconUrl";
+        internal const string PACKAGE_ICON_URL = "umbPackage/info/package/iconUrl";
+        internal const string AUTHOR_NAME_XPATH = "umbPackage/info/author/name";        
         internal const string AUTHOR_URL_XPATH = "umbPackage/info/author/website";
         internal const string README_XPATH = "umbPackage/info/readme";
     }
